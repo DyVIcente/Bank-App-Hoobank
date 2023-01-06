51a83cc0e8641f6611586ecc4c497783c0ca4aa0
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2023-01-06 17:05:48 +0100

    in progress hero getstarted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import styles from '../style'
+import { arrowUp } from '../assets'
 
 const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
+  return <div>GetStarted</div>
 }
 
 export default GetStarted
